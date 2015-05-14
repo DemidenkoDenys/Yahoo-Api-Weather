fc01fed355448091c11727f0c594fda7e20e2b09
Paul <paul.k@bigdropinc.com>
2015-05-14 14:52:27 +0300

    Add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -32,6 +32,7 @@ var cmq = require('gulp-combine-media-queries');
 var cssMin = require('gulp-cssmin');
 var csscomb = require('gulp-csscomb');
 var rigger = require('gulp-rigger');
+var sourcemaps = require('gulp-sourcemaps');
 
 var pages_ = require('./pages.json');
 
@@ -39,114 +40,118 @@ var pages_ = require('./pages.json');
 //HTML include
 
 var pages = pages_.map(function (a) {
-    return '_' + a + '.html';
+  return '_' + a + '.html';
 });
-var pagesAdd = pages;
+var pagesAdd = pages.map(function (a) {
+  return a;
+});
 pagesAdd.push('templates/*.html');
 
 gulp.task('htmlimport', function () {
-    gulp.src(pages)
-        .pipe(rigger())
-        .pipe(rename(function (path) {
-            var newName = path.basename;
-            if (newName.charAt(0) === '_')
-                newName = newName.slice(1);
-            path.basename = newName;
-        }))
-        .pipe(gulp.dest(''));
+  gulp.src(pages)
+    .pipe(rigger())
+    .pipe(rename(function (path) {
+      var newName = path.basename;
+      if (newName.charAt(0) === '_')
+        newName = newName.slice(1);
+      path.basename = newName;
+    }))
+    .pipe(gulp.dest(''));
 });
 
 // Images, Fonts
 
 gulp.task('imgmin', function () {
-    return gulp.src('assets/images/**')
-        .pipe(imageMin({
-            progressive: true,
-            svgoPlugins: [
-                {removeViewBox: false}
-            ],
-            use: [pngCrush()]
-        }))
-        .pipe(gulp.dest('dist/images'));
+  return gulp.src('assets/images/**')
+    .pipe(imageMin({
+      progressive: true,
+      svgoPlugins: [
+        {removeViewBox: false}
+      ],
+      use: [pngCrush()]
+    }))
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('fonts', function () {
-    gulp.src('assets/fonts/**')
-        .pipe(gulp.dest('dist/fonts'));
+  gulp.src('assets/fonts/**')
+    .pipe(gulp.dest('dist/fonts'));
 });
 
 // SCSS
 
 gulp.task('scss', function () {
-    gulp.src(['assets/css/global.scss', 'assets/css/pages/*.scss'])
-        .pipe(sass())
-        .pipe(prefix('last 2 versions', '> 1%', 'ie 9'))
-        .pipe(cmq({
-            log: true
-        }))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(csscomb())
-        .pipe(cssMin())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist/css'));
+  gulp.src(['assets/css/global.scss', 'assets/css/pages/*.scss'])
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(prefix('last 2 versions', '> 1%', 'ie 9'))
+    .pipe(cmq({
+      log: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(csscomb())
+    .pipe(cssMin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('dist/css'));
 });
 
 // JS
 
 function getFiles(dir) {
-    return fs.readdirSync(dir)
-        .filter(function (file) {
-            return fs.statSync(path.join(dir, file)).isFile();
-        });
+  return fs.readdirSync(dir)
+    .filter(function (file) {
+      return fs.statSync(path.join(dir, file)).isFile();
+    });
 }
 
 function mapJs(callback) {
-    if (typeof callback !== 'function') {
-        return;
-    }
-    getFiles('assets/js')
-        .forEach(function (file) {
-            file = file.slice(0, -3);
-            callback(file);
-        });
+  if (typeof callback !== 'function') {
+    return;
+  }
+  getFiles('assets/js')
+    .forEach(function (file) {
+      file = file.slice(0, -3);
+      callback(file);
+    });
 }
 
 gulp.task('jscs', function () {
-    mapJs(function (file) {
-        gulp.src(['assets/js/' + file + '.js'])
-            .pipe(jscs());
-    });
+  mapJs(function (file) {
+    gulp.src(['assets/js/' + file + '.js'])
+      .pipe(jscs());
+  });
 });
 
 gulp.task('lint', function () {
-    mapJs(function (file) {
-        gulp.src(['assets/js/' + file + '.js'])
-            .pipe(jshint('.jshintrc'))
-            .pipe(jshint.reporter('jshint-stylish'));
-    });
+  mapJs(function (file) {
+    gulp.src(['assets/js/' + file + '.js'])
+      .pipe(jshint('.jshintrc'))
+      .pipe(jshint.reporter('jshint-stylish'));
+  });
 });
 
 gulp.task('js', ['jscs', 'lint'], function () {
-    mapJs(function (file) {
-        gulp.src(['assets/js/' + file + '/**', 'assets/js/' + file + '.js'])
-            .pipe(concat(file + '.js'))
-            .pipe(gulp.dest('dist/js'))
-            .pipe(uglify())
-            .pipe(rename({
-                suffix: '.min'
-            }))
-            .pipe(gulp.dest('dist/js'));
-    });
+  mapJs(function (file) {
+    gulp.src(['assets/js/' + file + '/**', 'assets/js/' + file + '.js'])
+      .pipe(concat(file + '.js'))
+      .pipe(gulp.dest('dist/js'))
+      .pipe(uglify())
+      .pipe(rename({
+        suffix: '.min'
+      }))
+      .pipe(gulp.dest('dist/js'));
+  });
 });
 
 // WATCH
 
 gulp.task('watch', function () {
-    gulp.watch('assets/js/**/*.js', ['js']);
-    gulp.watch(pagesAdd, ['htmlimport']);
-    gulp.watch('assets/css/**/*.scss', ['scss']);
+  gulp.watch('assets/js/**/*.js', ['js']);
+  gulp.watch(pagesAdd, ['htmlimport']);
+  gulp.watch('assets/css/**/*.scss', ['scss']);
 });
 
 // DEFAULT
