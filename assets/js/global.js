d5a5423824f4b12b0e563abc9c15736c0bc02a55
paul <paul.k@bigdropinc.com>
2016-04-22 15:22:26 +0300

    add example JS for WP

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,65 +1,49 @@
 // DO NOT REMOVE THIS!! This polyfill provides correct work in IE while we are using Babel to transform our JS.
 import "babel-polyfill";
-import {foo} from "./modules/foo";
-import {a as myArr} from "./modules/vars";
-
+import HOME from "./modules/HOME";
+import ABOUT from "./modules/ABOUT";
 
 // JUST EXAMPLE OF USAGE! Import jQuery from node_modules and set global scope.
 // REMOVE ALL THIS FOR YOUR PROJECT
 global.jQuery = require('jquery');
 global.$ = global.jQuery;
 
-
 console.info(`Environment: ${process.env.NODE_ENV}`, process.env);
-
-const GLOBAL = {
-    home_init () {
-        let {a, b} = myArr;
-        console.log(`${a}, ${b} `, myArr);
-        const executeFunction = foo(a, b);
-        console.log(executeFunction);
-        console.log(this);
-        this.example_wp_ajax();
-    },
-    load () {
-    },
-    resize () {
-    },
-    scroll () {
-    },
-    example_wp_ajax () {
-        console.log(vars);
-        $.ajax({
-            type: 'GET',
-            url: vars.ajaxUrl,
-            data: {
-                action: 'add_content'
-            },
-            dataType: 'html',
-            success (response) {
-                $('#example_wp_ajax').html(process.env.NODE_ENV);
-                if (global.vars.example_wp_ajax_callback) {
-                    global.vars.example_wp_ajax_callback(response);
-                }
-            },
-            error: function (response) {
-                console.error(response.responseText);
-            }
-        });
-    }
-};
-
-
-let init = null;
-
-switch (global.vars.page) {
-    case 'home_page':
-        init = GLOBAL.home_init.bind(GLOBAL);
-        break;
-    case 'about_page':
-        init = GLOBAL.about_init.bind(GLOBAL);
-    default:
-        init = () => console.error('Init is not a function');
+if (process.env.NODE_ENV === 'development') {
+    console.log('Тут я могу писать шо хочу - в продакшене это удалиться!')
 }
 
+
+/**
+ * Создаем функцию init, которая
+ * будет вызываться в любом случае
+ */
+let init = null;
+/**
+ * Перебираем window.vars.page,
+ * чтобы выяснить, какая у нас страница
+ */
+switch (global.vars.page) {
+    case 'home_page':
+        init = HOME.init.bind(HOME);
+        break;
+    case 'about_page':
+    case 'contact_page':
+        init = ABOUT.init.bind(ABOUT);
+        break;
+    default:
+        init = () => {
+            console.log('default init');
+        };
+}
+/**
+ * Вешаем на document.onready нашу инициализацию страницы
+ */
 $(document).ready(init);
+
+
+
+
+
+
+
--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -2,13 +2,11 @@
 
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
-var options = require('../config');
 
 gulp.task('dev', function(cb) {
 
     cb = cb || function() {};
 
-    // global.production = false;
     process.env.NODE_ENV === 'development';
     runSequence(['scss', 'js', 'htmlimport', 'watch'], cb);
 
